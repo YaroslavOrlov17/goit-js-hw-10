2f4393f09d9e84c98561ee5d44c891289e2c1d5d
Yaroslav Orlov <sanenss@gmail.com>
2024-07-17 13:09:44 +0300

    snackbar add

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const delayInput = document.querySelector("input[name=delay]")
+
+const form = document.querySelector(".form")
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const state = document.querySelector('input[name="state"]:checked').value;
+    const delay = Number(delayInput.value);
+    if (state === "fulfilled") {
+        Promise.resolve(delay)
+            .then(value => setTimeout(() => iziToast.success({
+                position: "topRight",
+                messageColor: "white",
+                backgroundColor: "green",
+                message: `✅ Fulfilled promise in ${value}ms`
+            }), value))
+    } else if (state === "rejected") {
+        Promise.reject(delay)
+            .catch(value => setTimeout(() => iziToast.error({
+                position: "topRight",
+                messageColor: "white",
+                backgroundColor: "red",
+                message: `❌ Rejected promise in ${value}ms`
+            }), value))
+    }
+}
+
+form.addEventListener("submit", handleSubmit)
+
